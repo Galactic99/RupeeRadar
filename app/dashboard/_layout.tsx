0261612c6bd6c9f415efc00c9b37f6fe4bb22548
Galactic99 <abhinavashok2004@gmail.com>
2025-04-20 22:44:19 +0530

    removed overview

--- app/dashboard/_layout.tsx
+++ app/dashboard/_layout.tsx
@@ -4,16 +4,7 @@ import { Ionicons } from "@expo/vector-icons";
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Overview",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home" size={size} color={color} />
-          ),
-        }}
-      />
+    <Tabs screenOptions={{ headerShown: false }} initialRouteName="expenses">
       <Tabs.Screen
         name="expenses"
         options={{
